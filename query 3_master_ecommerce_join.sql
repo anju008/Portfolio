b6dd0299adacc2b7c13cfc43062f8586f0293b82
Anju SHAJI <74889684+anju008@users.noreply.github.com>
2026-04-06 12:49:05 +0200

    Create query 3_master_ecommerce_join.sql

---
+++ query 3_master_ecommerce_join.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.name,
+  p.category,
+  COUNT(oi.id) AS units_sold,
+  ROUND(SUM(oi.sale_price), 2) AS total_revenue
+FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+JOIN `bigquery-public-data.thelook_ecommerce.products` p ON oi.product_id = p.id
+WHERE oi.status = 'Complete'
+GROUP BY p.name, p.category
+ORDER BY total_revenue DESC
+LIMIT 10
